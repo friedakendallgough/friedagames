377e758d8f3aa159ea7542ba65c3b733d1e26687
friedakendallgough <friedakendallgough@gmail.com>
2019-06-12 15:56:47 +0000

    First commit.

---
+++ passageofdoom.py
@@ -0,0 +1,57 @@
+while True:
+
+    name = input("what is your name? ")
+
+    print("Hello " + name + ". Welcome to the game.")
+
+    tool = input("You have a choice of three objects, A. a rope B. a filter water bottle or C. a paddle ")
+
+    if tool == 'A':
+
+        rope = True
+
+    elif tool == 'B':
+
+        bottle = True
+
+    elif tool == 'C':
+
+        paddle = True
+
+    print("We are on an adventure in the Amazon rainforest")
+
+    so = input("Do you like adventures? ")
+
+    if so == 'no':
+
+        print("STUPID BUMFACE!!!GAME OVER.")
+
+        continue
+
+    abyss = input("You start walking and soon come to an abyss 500 miles wide and 500 miles deep. You can either A. cross the abyss using an extremely broken old rope bridge or B. walk around the abyss. ")
+
+    if abyss == 'A':
+
+        print("You start walking over the bridge realising too late that the bridge is fraying. you desperately try to get off the bridge but you only make it a couple of steps before it breaks completely and you plummet to the ground screamin your last words. GAME OVER.")
+
+        continue
+
+    if abyss == 'B':
+
+        print("You get one mile round the abyss and spot a perfectly new bridge that was previously not visible. You cross the bridge with no trouble and continue on your way.")
+
+    tribe = input("you reach a tribal village, they offer you food water and a place to sleep but you are troubled by all the weapons on the walls. you can A. stay the night with the tribe or B. go and find somewhere else to sleep.")
+
+    if tribe == 'B':
+
+        print("You walk away from the village quickening your pace when you realise how dark it is. you eventually find a cave and settle down for the night. you wake up in the middle of the night with a start thinking you heard something, you listen for a while but don't hear anything. thinking you were imagining it you begin to close your eyes, but then you hear it again and this time you know your not imagining it. you slowly sit up starting to wish you'd stayed with the tribe when suddenly something leaps out of the darkness at you. you try to fight it off but it grabs your leg and starts dragging you itno the trees leaving a trail of blood in it's wake the echoes of your screams fading into the night. GAME OVER.")
+
+        continue
+
+
+
+
+    
+
+    
+
